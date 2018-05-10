61fc6390c7aaf056b0e5b11fbe10bc897e7875a2
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-10 12:58:42 +0200

    fix small issue

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 routes(app);
 
