132743de8107e5b7b37d1ce4c067b51c2c3deba0
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-10 12:46:46 +0200

    fix port for heroku

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes.js");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-const server = app.listen(3000, function () {
-  console.log("app running on port.", server.address().port);
+const server = app.listen(port, () => {
+  console.log("app running on port.", port);
 });
